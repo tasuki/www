8fa0bf3190b39cd99d58f80a187b6e8808e481f0
Vit Brunner <vit.brunner@gmail.com>
2013-03-16 22:34:42 +0000

    Merge h1 and h4 animations

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ $weights = array(
 	<script>
 		$(document).ready(function() {
 			function reanimate() {
-				var timeout = 10000, low  = 200, high = 255, dark = light = '';
+				var timeout = 10000,  low = 200, high = 255, dark = light = '';
 				for (i = 0; i < 3; i++) {
 					color = Math.floor(Math.random() * (high - low)) + low;
 					light += color.toString(16);
@@ -62,9 +62,8 @@ $weights = array(
 				light = "#" + light;
 				dark = "#" + dark;
 
+				$("h1, h4").animate({ backgroundColor: dark, color: light }, timeout);
 				$("body").animate({ backgroundColor: light, color: dark }, timeout);
-				$("h1").animate({ backgroundColor: dark, color: light }, timeout);
-				$("h4").animate({ backgroundColor: dark, color: light }, timeout);
 				$("a").animate({ color: dark }, timeout, function() {
 					reanimate();
 				});
