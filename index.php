358e5920e9cb0353f6de9395c0a1d0c796f477d6
Vit Brunner <vit.brunner@gmail.com>
2013-04-27 20:06:54 +0100

    Fix html validation

--- index.php
+++ index.php
@@ -48,9 +48,10 @@ $weights = array(
 		a:hover { text-decoration: none; }
 		a strong { font-weight: <?php echo $weights['category'] ?>; }
 	</style>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
-	<script>
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
+	<script type="text/javascript">
+		//<![CDATA[
 		$(document).ready(function() {
 			function reanimate() {
 				var timeout = 10000,  low = 200, high = 255, dark = light = '';
@@ -70,8 +71,7 @@ $weights = array(
 			}
 			reanimate();
 		});
-	</script>
-	<script type="text/javascript">
+
 		var _gaq = _gaq || [];
 		_gaq.push(['_setAccount', 'UA-2554217-7']);
 		_gaq.push(['_trackPageview']);
@@ -80,6 +80,7 @@ $weights = array(
 			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 		})();
+		//]]>
 	</script>
 </head>
 <body>
