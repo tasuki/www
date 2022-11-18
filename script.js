cf69f1e980aaeb81c5d9322e80b5157876c29955
Vit Brunner <vit.brunner@gmail.com>
2022-11-18 15:03:22 +0100

    Goodbye jQuery and PHP

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function(e) {
 	function colorHex(color) {
 		return color.toString(16).padStart(2, "0");
 	}
+
+	const header = document.querySelector("#header");
+	const body = document.querySelector("body");
+
 	function reanimate() {
 		low = 200, high = 255, dark = light = "#";
 		for (i = 0; i < 3; i++) {
@@ -10,11 +14,10 @@ $(document).ready(function() {
 			light += colorHex(color + low);
 		}
 
-		$(".header").css("backgroundColor", dark);
-		$("body").css("backgroundColor", light);
-		$(".header").css("color", light);
-		$("body").css("color", dark);
-		$("a").css("color", dark);
+		header.style.backgroundColor = dark;
+		body.style.backgroundColor = light;
+		header.style.color = light;
+		body.style.color = dark;
 	}
 
 	reanimate();
